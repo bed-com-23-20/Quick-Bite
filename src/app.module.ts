4931d2645c0acf335f0cee18f7642be4ebc5dc46
bed-com-23-20 <bed-com-23-20@unima.ac.mw>
2023-06-28 17:52:51 +0200

    Upgrading the database account

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { CommentTable } from './user/entities/comment.entity';
     host: 'sql10.freesqldatabase.com',
     port: 3306,
     username: 'sql10627017',
-    password: '2ELJ2yh8Jn',
+    password: '2ELJ2yh8Jn',  
     database: 'sql10627017',
     entities: [User, Account,Ordertable, CommentTable],
     synchronize: true,
