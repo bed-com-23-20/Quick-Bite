f0c614bd622ce5e4b72c32325a2c620c3d98f6e1
bed-com-23-20 <bed-com-23-20@unima.ac.mw>
2023-06-29 14:36:38 +0200

    Re-activating account

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,9 @@ import { CommentTable } from './user/entities/comment.entity';
     type: 'mysql',
     host: 'sql10.freesqldatabase.com',
     port: 3306,
-    username: 'sql10627017',
-    password: '2ELJ2yh8Jn',  
-    database: 'sql10627017',
+    username: 'sql10629473',
+    password: 'VTnLDZAXXt',  
+    database: 'sql10629473',
     entities: [User, Account,Ordertable, CommentTable],
     synchronize: true,
 
